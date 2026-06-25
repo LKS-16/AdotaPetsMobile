aac3bc900eddd8d59450fa8680ce112166f0274a
LKS-16 <araujolukas093@gmail.com>
2026-06-25 10:15:03 -0300

    adição da tela de carregamento

---
+++ lib/view/pages/tela_carregamento.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class TelaCarregamento extends StatefulWidget {
+  const TelaCarregamento({super.key});
+
+  @override
+  State<TelaCarregamento> createState() => _TelaCarregamentoState();
+}
+
+class _TelaCarregamentoState extends State<TelaCarregamento>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _escalaAnimation;
+  late Animation<double> _opacidadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 900),
+    )..repeat(reverse: true);
+
+    _escalaAnimation = Tween<double>(begin: 0.85, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    _opacidadeAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F2EE),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Ícone animado
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (_, __) {
+                return Transform.scale(
+                  scale: _escalaAnimation.value,
+                  child: Opacity(
+                    opacity: _opacidadeAnimation.value,
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFE8622A),
+                        borderRadius: BorderRadius.circular(22),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xFFE8622A).withOpacity(0.3),
+                            blurRadius: 20,
+                            spreadRadius: 4,
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.pets,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+
+            const SizedBox(height: 24),
+
+            // Texto de carregamento
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (_, __) {
+                return Opacity(
+                  opacity: _opacidadeAnimation.value,
+                  child: const Text(
+                    'Carregando...',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF888888),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
